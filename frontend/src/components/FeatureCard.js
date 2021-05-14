900c7d900bee605b471914b8abeae6f29c549e3c
Alex Chow <alex0091845@gmail.com>
2021-05-14 11:20:18 -0700

    added three panels and cleaned up cards

--- frontend/src/components/FeatureCard.js
+++ frontend/src/components/FeatureCard.js
@@ -1,112 +1,9 @@
 import React from "react";
 import "../styles/FeatureCard.css";
-import axios from "axios";
-import { SearchDisplayList, SearchDisplayItem } from "../components/SearchDisplayList";
-import { get } from '../utils/api';
 class FeatureCard extends React.Component{
     constructor(props) {
         super(props);
-        
-        this.state = {
-            isCheck : false,
-            query: '',
-            results: {},
-            loading: false,
-            message: '',
-            // just for where to put the search results
-            searchStyle: {
-                top: 0,
-                left: 0,
-                width: 0
-            }
-        };
-
-		this.cancel = '';
     }
-
-    // TODO: maybe delete this if don't need it
-    handleFocus = (e) => {
-        let rect = e.target.getBoundingClientRect();
-        this.setState({ searchStyle: { top: rect.bottom, left: rect.left, width: rect.width } });
-        this.setState({ searchAppear: true });
-    }
-    
-    // TODO: maybe delete this if don't need it
-    handleBlur = (e) => {
-        this.setState({ searchAppear: false });
-    }
-
-    handleOnInputChange = (e) => {
-			const query = e.target.value;
-			if ( ! query ) {
-				this.setState({ query, results: {}, message: '' } );
-			} else {
-				this.setState({ query, loading: true, message: '' }, () => {
-					this.fetchSearchResults(query);
-				});
-			}
-		};
-
-	fetchSearchResults = (query) => {
-    const searchUrl = `https://api.spotify.com/v1/search?query=${encodeURIComponent(query)}&type=album,playlist,artist`;
-		if (this.cancel) {
-			// Cancel the previous request before making a new request
-			this.cancel.cancel();
-		}
-		// Create a new CancelToken
-		this.cancel = axios.CancelToken.source();
-
-		get(searchUrl, {
-				cancelToken: this.cancel.token,
-			})
-			.then((res) => {
-				console.log(res);
-				this.setState({
-					results: res,
-					loading: false,
-				});
-			})
-			.catch((error) => {
-				if (axios.isCancel(error) || error) {
-					this.setState({
-						loading: false,
-						message: 'Failed to fetch results.Please check network',
-					});
-				}
-			});
-	};
-
-	renderSearchResults = () => {
-		const {results} = this.state;
-		if (Object.keys(this.state.results).length && 
-				this.state.results.albums.items.length) {
-			
-			// get the first three songs
-			const songs = [
-				this.state.results.albums.items[0],
-				this.state.results.albums.items[1],
-				this.state.results.albums.items[2],
-			];
-
-			// create the search display items from each song
-			const items = songs.map(song => {
-				return <SearchDisplayItem 
-					songName={song.name}
-					artist={song.artists[0].name}
-					key={song.id}
-				/>
-			})
-
-			return (
-				<div className="results-container">
-					<SearchDisplayList 
-						items={ items } 
-						style={this.state.searchStyle}
-					/>
-				</div>
-			);
-		}
-	};
 
     render(){
 
@@ -118,23 +15,11 @@ class FeatureCard extends React.Component{
 									{this.props.feature}
 							</label>
 
-							{/* Song text label */}
+							{/* Songs text label */}
 							<div className="song">
-									<p>Song: </p>
+									<p>Songs: </p>
 							</div>
 
-							{/* Input text field */}
-							<input
-									type="text"
-									placeholder="Search for a song..."
-									className="text-field"
-									onChange={this.handleOnInputChange}
-									onFocus={e => this.handleFocus(e)}
-									onBlur={e => this.handleBlur(e)}
-							/>
-
-							{/* Search Result field */}
-							{ this.renderSearchResults() }
 					</div>
         );
     }
